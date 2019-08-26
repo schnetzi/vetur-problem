364665dc7f8f7180ab5311cede579f21028f34e9
schnetzi <paul.schnetzinger@gmail.com>
2019-08-26 15:20:02 +0100

    Change to renderless component

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,70 +1,23 @@
-<template>
-	<div class="c-testcomponent">
-		Testcomponent {{ isCallOfMandatoryMethodWorking(myTest) }}
-	</div>
-</template>
-
 <script lang="ts">
 import Vue from 'vue';
-import { ITestComponent } from '@/interfaces/testComponent';
 
 export default Vue.extend({
 	name: 'TestComponent',
 	props: {
-		myTest: {
-			type: Object as () => ITestComponent,
-			required: true,
-		},
 		scenarios: {
-			type: Array,
-			required: true,
-		},
-		isChecked: {
-			type: Boolean,
+			type: [],
 			required: true,
 		},
 	},
 	data() {
 		return {
-			isCheckedChild: (this.isCallOfMethodWorking(this.myTest) ||
-				this.isChecked) as boolean,
+			testData: true as boolean,
 		};
 	},
-	computed: {
-		computedMethod(): any {
-			return {
-				state: 'start',
-			};
-		},
-		currentTest(): boolean {
-			const state = this.computedMethod.state;
-			if (state === 'start') {
-				return true;
-			} else {
-				return false;
-			}
-		},
-	},
-	methods: {
-		isCallOfMethodWorking(gameplay: ITestComponent): any {
-			return (
-				gameplay.Mode === 'optional' || gameplay.Mode === 'mandatory'
-			);
-		},
-		isCallOfMandatoryMethodWorking(gameplay: ITestComponent): boolean {
-			if (this.isCheckedChild) {
-				console.log('yes');
-			}
-
-			return gameplay.Mode === 'mandatory';
-		},
+	render() {
+		return this.$scopedSlots.default({
+			data: this.testData,
+		});
 	},
 });
 </script>
-
-<style lang="scss" scoped>
-.c-testcomponent {
-	border: 1px solid deeppink;
-	padding: 20px;
-}
-</style>
